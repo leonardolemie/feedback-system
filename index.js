c8a2e8142e68bb4696f48c75bdc214c81039339d
Leonardo Bernardes <leonardolemie@gmail.com>
2017-08-23 00:35:54 -0300

    Added google oauth2

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const passport = require ('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
+const keys = require('./config/keys');
 
-app.get("/", (req, res) =>{
-  res.send({ hi: 'there'});
-});
+passport.use(new GoogleStrategy({
+  clientID: keys.googleClientId,
+  clientSecret: keys.googleClientSecret,
+  callbackURL: '/auth/google/callback',
+}, (accessToken, refreshToken, profile, done) => {
+  console.log(accessToken);
+  console.log(refreshToken);
+  console.log(profile);
+  console.log(done);
+}));
+
+app.get('/auth/google',passport.authenticate('google',{
+  scope: ['profile', 'email']
+}));
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 
