7f186af7cf712192fdc11cdb7a0c55f5814e9cb4
Leonardo Bernardes <leonardolemie@gmail.com>
2017-08-27 17:34:47 -0300

    Authentication with cookies savingit with mongodb

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,4 +6,14 @@ module.exports = (app) => {
   }));
 
   app.get('/auth/google/callback', passport.authenticate('google'));
+
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
+
+  app.get('/api/currentUser', (req, res) => {
+    res.send(req.user);
+  })
+
 };
