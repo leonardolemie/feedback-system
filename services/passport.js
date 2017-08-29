8a1468c721fcbf52ab9f0e2c950ac6da7294b7e8
Leonardo Bernardes <leonardolemie@gmail.com>
2017-08-28 21:54:55 -0300

    Fix proxy issues with heroku

--- services/passport.js
+++ services/passport.js
@@ -14,12 +14,13 @@ passport.deserializeUser((id, done) => {
     .then(user => {
       done(null, user);
     })
-})
+});
 
 passport.use(new GoogleStrategy({
   clientID: keys.googleClientId,
   clientSecret: keys.googleClientSecret,
   callbackURL: '/auth/google/callback',
+  proxy: true
 }, (accessToken, refreshToken, profile, done) => {
   User.findOne({ googleId : profile.id}).then((existingUser) => {
     if (existingUser) {
