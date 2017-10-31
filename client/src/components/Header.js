01026156c862a9bf0912fce88e0d380856208346
Leonardo Bernardes <leonardo.bernardes@sap.com>
2017-10-31 08:08:54 -0200

    Added header component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,16 @@ import React, { Component } from 'react';
 
 class Header extends Component {
   render() {
-    return <div>Header</div>;
+    return(
+    <nav>
+      <div className="nav-wrapper">
+        <a href="#" className="left brand-logo">Emaily</a>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li><a href="#">Login with Google</a></li>
+        </ul>
+      </div>
+    </nav>)
+    ;
   }
 }
 
