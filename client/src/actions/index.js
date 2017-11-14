5ef74acf5ea8ae189f72ac7d9c099dc18c367279
Leonardo Bernardes <leonardo.bernardes@sap.com>
2017-11-14 09:02:48 -0200

    Billing service

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,8 +1,17 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
+// curried functions
+//https://stackoverflow.com/questions/32782922/what-do-multiple-arrow-functions-mean-in-javascript
+
 export const fetchUser = () => async dispatch => {
   const res = await axios.get('/api/currentUser');
   console.log(res.data);
+  dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const handleToken = token => async dispatch => {
+  const res = await axios.post('/api/stripe', token);
+
   dispatch({ type: FETCH_USER, payload: res.data });
 };
